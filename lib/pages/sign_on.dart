b341ba60145c3b0fba6677dcc69712e1c01fcd1b
Colby Wang <colby.wang765@gmail.com>
2024-01-24 14:44:19 -0500

    remove unneeded import

--- lib/pages/sign_on.dart
+++ lib/pages/sign_on.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cord2_mobile_app/pages/chat.dart';
 import 'package:cord2_mobile_app/main.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
