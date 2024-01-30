98151772d1f2265fe19a05a3972bc87c7651f942
MendyJ <97720461+MendyJ@users.noreply.github.com>
2024-01-30 17:58:00 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cord2_mobile_app/pages/sign_on.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -33,7 +31,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   String currentPage = "Map"; // Track the current page
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -97,10 +97,52 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
+    return MaterialApp(
+      title: 'CoRD2 Mobile Application',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff5F79BA)),
+        useMaterial3: true,
+      ),
+      home: const SignOnPage(),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search Overlay Example'),
+      ),
+      body: Center(
+        child: Text('Your main content goes here'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(top: 30.0),
+        child: ElevatedButton(
+          onPressed: () {
+            showSearch(
+              context: context,
+              delegate: CustomSearchDelegate(),
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            primary: Theme.of(context).primaryColor,
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Search...',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(width: 50.0),
+              Icon(Icons.search, color: Colors.white),
+            ],
+          ),
+        ),
       ),
     );
   }
-
   Widget _buildDrawerItem(String pageName) {
     return ListTile(
       title: Text(pageName),
@@ -133,5 +175,107 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       currentPage = pageName;
     });
+  }
+}
+
+class CustomSearchDelegate extends SearchDelegate<String> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, "");
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return buildSuggestions(context);
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('events').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return CircularProgressIndicator();
+        }
+
+        final List<Map<String, dynamic>> suggestionList = query.isEmpty
+            ? (snapshot.data as QuerySnapshot)
+                .docs
+                .where((doc) => doc['active'] == true)
+                .map((doc) => {
+                      'title': doc['title'] as String,
+                      'description': doc['description'] as String,
+                    })
+                .toList()
+            : (snapshot.data as QuerySnapshot)
+                .docs
+                .where((doc) => doc['active'] == true)
+                .map((doc) => {
+                      'title': doc['title'] as String,
+                      'description': doc['description'] as String,
+                    })
+                .where((element) =>
+                    element['title'] != null &&
+                    element['title']!
+                        .toLowerCase()
+                        .contains(query.toLowerCase()))
+                .toList();
+
+        return ListView.builder(
+          itemCount: suggestionList.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(suggestionList[index]['title'] ?? ''),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ReportPage(
+                      description: suggestionList[index]['description'] ?? '',
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class ReportPage extends StatelessWidget {
+  final String description;
+
+  ReportPage({required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Report Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(description),
+      ),
+    );
   }
 }
