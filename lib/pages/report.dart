513e13811e81b78224e021dc337b0297fab2b3df
tarina49 <122750195+tarina49@users.noreply.github.com>
2024-02-09 13:58:33 -0500

    Image uploads

--- lib/pages/report.dart
+++ lib/pages/report.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+//import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -20,6 +22,8 @@ class ReportForm extends StatefulWidget {
 class _ReportFormState extends State<ReportForm> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   String get currentUserId => widget.userId ?? "";
+  String imageUrl = '';
+  List<String> imageUrls = [];
 
   @override
   void initState() {
@@ -34,13 +38,28 @@ class _ReportFormState extends State<ReportForm> {
   String _error = "";
 
   Future<void> pickImage() async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    ImagePicker picker = ImagePicker();
+    XFile? file = await picker.pickImage(source: ImageSource.camera);
 
-    if (pickedFile != null) {
-      setState(() {
-        _imageFile = File(pickedFile.path);
-      });
+    if (file == null) {
+      return;
+    }
+
+    print('Image path: ${file.path}');
+
+    String uniqueFileName = DateTime.now().millisecondsSinceEpoch.toString();
+    print('uniquefilename ${uniqueFileName}');
+    Reference referenceRoot = FirebaseStorage.instance.ref();
+    Reference referenceDirImages = referenceRoot.child('images');
+    Reference referenceImageToUpload = referenceDirImages.child(uniqueFileName);
+
+    try {
+      File imageFile = File(file!.path);
+      await referenceImageToUpload.putFile(imageFile);
+      imageUrl = await referenceImageToUpload.getDownloadURL();
+      print('Uploaded image URL: $imageUrl');
+    } catch (error) {
+      print('Error: $error');
     }
   }
 
@@ -61,11 +80,17 @@ class _ReportFormState extends State<ReportForm> {
       setError("Please add a title.");
       return;
     }
+    if(imageUrl.isEmpty){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Please upload an image!")));
+      return;
+    }
+
+    imageUrls.add(imageUrl);
 
     Map<String, dynamic> submissionData = {
       'description': descriptionCon.text,
       'creator': userId,
-      'images': [],
+      'images': imageUrls,
       'title': titleCon.text,
       'eventType': selectedCategory,
       'latitude': 28.544331,
@@ -73,146 +98,149 @@ class _ReportFormState extends State<ReportForm> {
       'time': DateTime.now(),
     };
 
-      try {
+    try {
+      // await _firestore.collection('users').doc(userId).update({
+      //   'events': FieldValue.arrayUnion([submissionData]),
+      // });
+      await _firestore.collection('events').add(submissionData)
+          .then((DocumentReference data) async {
         await _firestore.collection('users').doc(userId).update({
-          'events': FieldValue.arrayUnion([submissionData]),
+          'events': FieldValue.arrayUnion([data.id]),
         });
-        print('Submission saved successfully!');
-      } catch (e) {
-        print('Error saving submission: $e');
-      }
-}
+      });
+
+      print('Submission saved successfully!');
+    } catch (e) {
+      print('Error saving submission: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return
-    Container(
-      padding: const EdgeInsets.only(top: 0),
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Submit a Report',
-            style: TextStyle(
-              fontSize: 25.0,
-              fontWeight: FontWeight.w400,
-              color: Colors.black,
-            ),
-          ),
-          const SizedBox(height: 50),
-          Container(
-            //   height: MediaQuery.of(context).size.height-200,
-            padding: const EdgeInsets.only(top: 30),
-            decoration: const BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30.0),
-                topRight: Radius.circular(30.0),
+      Container(
+        padding: const EdgeInsets.only(top: 0),
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Submit a Report',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.w400,
+                color: Colors.black,
               ),
             ),
-            //  width: double.infinity,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Padding(
-                      padding: const EdgeInsets.only(
-                          top: 20, bottom: 30, right: 30, left: 30),
-                      child:
-                      TextField(
-                        controller: titleCon,
-                        decoration: const InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          // Set your desired background color
-                          labelText: 'Add a title.',
-                        ),
-                      )),
-                  const Text(
-                    'Category',
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
+            const SizedBox(height: 50),
+            Container(
+              //   height: MediaQuery.of(context).size.height-200,
+              padding: const EdgeInsets.only(top: 30),
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+              //  width: double.infinity,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20, bottom: 30, right: 30, left: 30),
+                        child:
+                        TextField(
+                          controller: titleCon,
+                          decoration: const InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            // Set your desired background color
+                            labelText: 'Add a title.',
+                          ),
+                        )),
+                    const Text(
+                      'Category',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10)),
-                      padding: EdgeInsets.only(right: 10, left: 10),
-                      child:
-                      DropdownButton<String>(
-                        style: const TextStyle(
-                            color: Colors.black
-                        ),
-                        dropdownColor: Colors.white,
-                        value: selectedCategory,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            selectedCategory = newValue!;
-                          });
+                    const SizedBox(height: 20.0),
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        padding: EdgeInsets.only(right: 10, left: 10),
+                        child:
+                        DropdownButton<String>(
+                          style: const TextStyle(
+                              color: Colors.black
+                          ),
+                          dropdownColor: Colors.white,
+                          value: selectedCategory,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedCategory = newValue!;
+                            });
+                          },
+                          items: <String>[
+                            'Hurricane',
+                            'Earthquake',
+                            'Tornado',
+                            'Wildfire',
+                            'Other'
+                          ]
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        )),
+                    const SizedBox(height: 40.0),
+                    const Text(
+                      'Description',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20, bottom: 30, right: 30, left: 30),
+                        child:
+                        TextField(
+                          controller: descriptionCon,
+                          decoration: const InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            // Set your desired background color
+                            labelText: 'Please provide more information.',
+                          ),
+                        )),
+                    ElevatedButton(
+                        onPressed: () {
+                          pickImage();
                         },
-                        items: <String>[
-                          'Hurricane',
-                          'Earthquake',
-                          'Tornado',
-                          'Wildfire',
-                          'Other'
-                        ]
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      )),
-                  const SizedBox(height: 40.0),
-                  const Text(
-                    'Description',
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
+                        child: Text('Pick Image'),
                     ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.only(
-                          top: 20, bottom: 30, right: 30, left: 30),
-                      child:
-                      TextField(
-                        controller: descriptionCon,
-                        decoration: const InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          // Set your desired background color
-                          labelText: 'Please provide more information.',
-                        ),
-                      )),
-                  _imageFile == null
-                      ? ElevatedButton(
-                    onPressed: () {
-                      pickImage();
-                    },
-                    child: Text('Pick Image'))
-                  : Image.file(
-                    _imageFile!,
-                    height: 200,
-                    width: 200,
-                    fit: BoxFit.cover,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      submitReport(currentUserId);
-                    },
-                    child: Text('Submit a report.'),
-                  )
-                ],
+                  /*  if(imageUrl.isNotEmpty){
+
+                    }*/
+                    ElevatedButton(
+                      onPressed: () {
+                        submitReport(currentUserId);
+                      },
+                      child: Text('Submit a report.'),
+                    )
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
-      ),
-    );
+            )
+          ],
+        ),
+      );
   }
 }
-
